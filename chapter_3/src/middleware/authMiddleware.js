2709da7c52043430d48a6ededc309e90a763fb6e
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-11-04 13:47:34 +0200

    finished chapter 3

--- chapter_3/src/middleware/authMiddleware.js
+++ chapter_3/src/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import db from "../db.js";
 
 function authMiddleware(req, res, next) {
   const token = req.headers["authorization"];
@@ -6,13 +7,20 @@ function authMiddleware(req, res, next) {
     return res.status(401).json({ message: "No Token provided!" });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: "Invalid token" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const getUser = db.prepare(`SELECT * FROM users WHERE id = (?)`);
+    const user = getUser.get(decoded.id);
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
     }
+
     req.userId = decoded.id;
     next();
-  });
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 }
 
 export default authMiddleware;
--- chapter_3/src/routes/todosRoutes.js
+++ chapter_3/src/routes/todosRoutes.js
@@ -10,11 +10,32 @@ router.get("/", (req, res) => {
   if (!todos) res.send({});
   res.send(todos);
 });
+
 router.post("/", (req, res) => {
   const { task } = req.body;
   const addTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`);
   const result = addTodo.run(req.userId, task);
   res.status(201).json({ id: result.lastInsertRowid, task, completed: 0 });
+});
+
+router.delete("/:id", (req, res) => {
+  const todoId = req.params.id;
+  const removeTodo = db.prepare(
+    `DELETE FROM todos WHERE user_id = ? AND id = ?`
+  );
+  const result = removeTodo.run(req.userId, todoId);
+  res.status(201).send(result);
+});
+
+router.put("/:id", (req, res) => {
+  const {id} = req.params;
+  const editTodo = db.prepare(
+    `UPDATE todos
+    SET completed = ?
+    WHERE user_id = ? AND id = ?`
+  );
+  const result = editTodo.run(1, req.userId, id);
+  res.status(201).send(result);
 });
 
 export default router;
